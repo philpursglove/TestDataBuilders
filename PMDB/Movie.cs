5cb5b03e0cccaee7b430b171c54c1c173f03efcf
Phil Pursglove <philpursglove@users.noreply.github.com>
2025-05-09 09:10:54 +0100

    Second iteration

--- PMDB/Movie.cs
+++ PMDB/Movie.cs
@@ -5,30 +5,35 @@ public class Movie
     public Guid Id { get; set; }
     public string Name { get; set; }
 
-    public Movie(Guid id, string name)
+    public string Genre { get; set; }
+
+    public int YearOfRelease { get; set; }
+
+    public Movie(Guid id, string name, string genre, int year)
     {
         Id = id;
         Name = name;
+        Genre = genre;
+        YearOfRelease = year;
     }
 }
-
 public class MovieTests
 {
     public void MovieTest()
     {
-        var movie = new Movie(Guid.NewGuid(), "Jaws");
+        var movie = new Movie(Guid.NewGuid(), "Jaws", "Thriller", 1975);
         // test goes here
     }
 
     public void MovieTest2()
     {
-        var movie = new Movie(Guid.NewGuid(), "Jaws 2");
+        var movie = new Movie(Guid.NewGuid(), "Jaws 2", "Thriller", 1978);
         // test goes here
     }
 
     public void MovieTest3()
     {
-        var movie = new Movie(Guid.NewGuid(), "Jaws 3-D");
+        var movie = new Movie(Guid.NewGuid(), "Jaws 3-D", "Thriller", 1983);
         // test goes here
     }
 }
