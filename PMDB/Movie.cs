df5dd8b6109f9f84ae162724e7258670f8aa4b58
Phil Pursglove <philpursglove@users.noreply.github.com>
2025-05-09 09:42:38 +0100

    Add The Meg

--- PMDB/Movie.cs
+++ PMDB/Movie.cs
@@ -16,6 +16,20 @@ public class Movie
     public int Duration { get; set; }
     public string Description { get; set; }
     public List<StarRating> Ratings { get; set; }
+
+    public Movie(Guid id, string name, string genre, int year, Person director, List<Person> writers, List<Person> actors, int duration, string description, List<StarRating> ratings)
+    {
+        Id = id;
+        Name = name;
+        Genre = genre;
+        YearOfRelease = year;
+        Director = director;
+        Writers = writers;
+        Actors = actors;
+        Duration = duration;
+        Description = description;
+        Ratings = ratings;
+    }
 }
 
 public class MovieTests
@@ -56,5 +70,15 @@ public class MovieTests
         // This test doesn't need to initialise anything, it just needs a valid movie instance
         var movie = new MovieBuilder().Build();
         // test
+    }
+
+    public void Add_The_Meg_For_Testing()
+    {
+        var movie = new Movie(Guid.NewGuid(), "The Meg", "Science Fiction", 2018, new Person("Jon Turteltaub"),
+            new List<Person> { new Person("Dean Georgans"), new Person("Jon Hoeber"), new Person("Erich Hoeber") },
+            new List<Person>
+                { new Person("Jason Statham"), new Person("Ruby Rose") }, 118,
+            "A giant prehistoric killer shark", new List<StarRating>());
+        // test goes here
     }
 }
